cfe40edf739e426e5e8f7b491c3f09dad9924c9a
Nico Gonzalez <nicog96@gmail.com>
2018-11-07 18:41:37 -0500

    Add more libraries, macros, & functions.

--- network.h
+++ network.h
@@ -1,18 +1,26 @@
 /* LIBRARIES */
 #include <stdio.h>
-#include <sys/socket.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <pthread.h>
 
 
 /* MACROS */
-#define SOCKET_DEFAULT 80
+#define SOCKET_DEFAULT 8765
 #define DICT_DEFAULT "dictionary.txt"
+#define DICT_BUF 128
+#define WORKER_BUF 1
+#define CLIENT_BUF 1
 
 
 /* GLOBAL VARS */
 FILE *DICTIONARY;
-int SOCKET;
+FILE *LOG;
+int LISTEN_PORT;
 
 
 /* STRUCTS */
@@ -21,4 +29,17 @@ typedef struct client{
     struct client *next;
 }client;
 
+
 /* FUNCTIONS */
+void enqueue(client *newclient, client **queue);
+client* pop(client **queue);
+_Bool isempty(client **queue);
+_Bool isfull(client **queue);
+size_t size(client **queue);
+void print(client **queue);
+client* newNode();
+void* threadFunction(void* id);
+_Bool lookup(char *word);
+void *worker();
+void *logger();
+int open_listenfd(int port);
