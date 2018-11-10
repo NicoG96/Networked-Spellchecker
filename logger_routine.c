028254c8ebfd64641df0db20bdcac622b57a96d0
Nico Gonzalez <nicog96@gmail.com>
2018-11-10 14:30:18 -0500

    Finish logger

--- logger_routine.c
+++ logger_routine.c
@@ -11,19 +11,29 @@ void *logger_routine(void *args){
         pthread_mutex_lock(&serv->log_mutex);
 
         //check if it's empty
-        while (&serv->log_count == 0) {
+        while (serv->l_read_ptr == serv->l_write_ptr && serv->log_count == 0) {
             pthread_cond_wait(&serv->log_not_empty, &serv->log_mutex);
         }
 
+        //get the log results
+        char *result = logs[serv->l_read_ptr];
 
+        //write results to log
+        fprintf(LOG, "%s\n", result);
+
+        //clear index
+        logs[serv->l_read_ptr] = NULL;
+
+        //increment the read pointer
+        serv->l_read_ptr = (++serv->l_read_ptr) % BUFFER_MAX;
+
+        //decrement the total log count
+        --serv->log_count;
 
         //signal that Q isn't full
         pthread_cond_signal(&serv->log_not_full);
 
         //unlock the queue
         pthread_mutex_unlock(&serv->log_mutex);
-
-        //print results to log file
-        //fprintf(LOG, "%s %s\n", word, iscorrect);
     }
 }
