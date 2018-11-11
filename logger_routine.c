198549977c41502c34814e778d7b44a64529fcc6
Nico Gonzalez <nicog96@gmail.com>
2018-11-11 02:39:34 -0500

    Create separate function to handle log removal

--- logger_routine.c
+++ logger_routine.c
@@ -3,9 +3,6 @@
 void *logger_routine(void *args){
     server *serv = args;
 
-    //create log file
-    LOG = fopen("log.txt", "w+");
-
     while(1) {
         //lock the log queue
         pthread_mutex_lock(&serv->log_mutex);
@@ -16,19 +13,10 @@ void *logger_routine(void *args){
         }
 
         //get the log results
-        char *result = logs[serv->l_read_ptr];
+        char *result = remove_log(serv);
 
         //write results to log
         fprintf(LOG, "%s\n", result);
-
-        //clear index
-        logs[serv->l_read_ptr] = NULL;
-
-        //increment the read pointer
-        serv->l_read_ptr = (++serv->l_read_ptr) % BUFFER_MAX;
-
-        //decrement the total log count
-        --serv->log_count;
 
         //signal that Q isn't full
         pthread_cond_signal(&serv->log_not_full);
@@ -36,4 +24,19 @@ void *logger_routine(void *args){
         //unlock the queue
         pthread_mutex_unlock(&serv->log_mutex);
     }
+}
+
+char *remove_log(server *serv) {
+    char *result = logs[serv->l_read_ptr];
+
+    //clear index
+    logs[serv->l_read_ptr] = (char *) calloc(1, sizeof(char *));
+
+    //increment the read pointer
+    serv->l_read_ptr = (++serv->l_read_ptr) % BUFFER_MAX;
+
+    //decrement the total log count
+    --serv->log_count;
+
+    return result;
 }
