14605a551503aed7bfd5b86939d84998fa928a37
Nico Gonzalez <nicog96@gmail.com>
2018-11-11 04:29:13 -0500

    Extend thread ability by wrapping whole file in a while loop - allows for threads to finish serving one client and start servicing another immediately
    Add keyword to safely exit entire program

--- worker_routine.c
+++ worker_routine.c
@@ -2,77 +2,82 @@
 
 void *worker_routine(void* args) {
     server *serv = args;
-
-    //lock clientQ
-    pthread_mutex_lock(&serv->client_mutex);
-
-    //if client Q is empty
-    while (serv->c_read_ptr == serv->c_write_ptr && serv->client_count == 0) {
-        pthread_cond_wait(&serv->client_not_empty, &serv->client_mutex);
-    }
-
-    //get socket from clientQ
-    int socket = remove_client(serv);
-
-    //send signal that Q is not full
-    pthread_cond_signal(&serv->client_not_full);
-
-    //unlock mutex
-    pthread_mutex_unlock(&serv->client_mutex);
-
     char *msgError = "Problem receiving message";
     int bytesReturned;
     char *res;
     char *word;
     char *prompt = ">";
 
-    //keep receiving words until the client disconnects
     while(1) {
-        send(socket, prompt, strlen(prompt), 0);
-        //zero-initialize var to receive word
-        word = calloc(DICT_BUF, 1);
+        //lock clientQ
+        pthread_mutex_lock(&serv->client_mutex);
 
-        //receive word
-        bytesReturned = (int) recv(socket, word, DICT_BUF, 0);
-        //printf("%s\n", word);
-
-        //if there was an error in the message reception
-        if (bytesReturned < 0) {
-            send(socket, msgError, strlen(msgError), 0);
-            continue;
+        //if client Q is empty
+        while (serv->c_read_ptr == serv->c_write_ptr && serv->client_count == 0) {
+            pthread_cond_wait(&serv->client_not_empty, &serv->client_mutex);
         }
 
-        //or if the client pressed escape key
-        if(word[0] == 27) {
-            send(socket, "Goodbye.\n", strlen("Goodbye.\n"), 0);
-            break;
+        //get socket from clientQ
+        int socket = remove_client(serv);
+
+        //send signal that Q is not full
+        pthread_cond_signal(&serv->client_not_full);
+
+        //unlock mutex
+        pthread_mutex_unlock(&serv->client_mutex);
+
+        //keep receiving words until the client disconnects
+        while(1) {
+            send(socket, prompt, strlen(prompt), 0);
+            //zero-initialize var to receive word
+            word = calloc(DICT_BUF, 1);
+
+            //receive word
+            bytesReturned = (int) recv(socket, word, DICT_BUF, 0);
+            //printf("%s\n", word);
+
+            //if there was an error in the message reception
+            if (bytesReturned < 0) {
+                send(socket, msgError, strlen(msgError), 0);
+                continue;
+            }
+
+            //or if the client pressed escape key
+            if(word[0] == 27) {
+                send(socket, "Goodbye.\n", strlen("Goodbye.\n"), 0);
+                break;
+            }
+
+            if(strcmp(word, "EXIT\r\n") == 0) {
+                exit(0);
+            }
+
+            //search for word, set the result equal to whether it was found or not
+            int iscorrect = lookup(word);
+            iscorrect ? (res = "OK\n") : (res = "MISSPELLED\n");
+
+            //print results to client
+            send(socket, res, strlen(res), 0);
+
+            //push the result to the log queue, get lock first
+            pthread_mutex_lock(&serv->log_mutex);
+
+            //check if the buffer is full
+            while(serv->l_write_ptr == serv->l_read_ptr && serv->log_count == BUFFER_MAX) {
+                pthread_cond_wait(&serv->log_not_full, &serv->log_mutex);
+            }
+
+            //write to the logQ
+            insert_log(serv, word, iscorrect);
+
+            //signal that log Q isn't empty
+            pthread_cond_signal(&serv->log_not_empty);
+
+            //unlock the mutex
+            pthread_mutex_unlock(&serv->log_mutex);
         }
-
-        //search for word, set the result equal to whether it was found or not
-        int iscorrect = lookup(word);
-        iscorrect ? (res = "OK\n") : (res = "MISSPELLED\n");
-
-        //print results to client
-        send(socket, res, strlen(res), 0);
-
-        //push the result to the log queue, get lock first
-        pthread_mutex_lock(&serv->log_mutex);
-
-        //check if the buffer is full
-        while(serv->l_write_ptr == serv->l_read_ptr && serv->log_count == BUFFER_MAX) {
-            pthread_cond_wait(&serv->log_not_full, &serv->log_mutex);
-        }
-
-        //write to the logQ
-        insert_log(serv, word, iscorrect);
-
-        //signal that log Q isn't empty
-        pthread_cond_signal(&serv->log_not_empty);
-
-        //unlock the mutex
-        pthread_mutex_unlock(&serv->log_mutex);
+        close(socket);
     }
-    close(socket);
     return NULL;
 }
 
